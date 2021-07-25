047c84b9df9e21f71e910cf44533fc44594d8085
psy082 <zhappy114@gmail.com>
2021-07-25 19:12:43 +0900

    feat: memoDetail page 구현 - 개별 메모의 번호/제목/태그/내용 표시 - 수정 버튼 구현. 수정 버튼 클릭 시 메모 수정 기능을 실행할 수 있는 에디터 화면으로 이동 - 삭제 버튼 구현. 삭제 버튼 클릭 시 메모 삭제 기능을 실행할 수 있는 에디터 화면으로 이동

--- src/Pages/MemoDetail/MemoDetail.jsx
+++ src/Pages/MemoDetail/MemoDetail.jsx
@@ -1,9 +1,49 @@
 import * as S from './MemoDetail.styles';
+import { useHistory } from 'react-router';
+import { useMemoDetail } from './hooks';
+import { TagBadgeBar, ActionButton } from '../../Common/Components';
+
+import { backSVG, editSVG, deleteSVG } from '../../assets/svg';
+
+const memoContents = ['id', 'title', 'tags', 'text'];
 
 const MemoDetail = () => {
+  const { memo, id } = useMemoDetail();
+  const history = useHistory();
+
+  const toList = () => {
+    history.push('/');
+  };
+
+  const editMemo = id => {
+    history.push(`/edit/${id}`);
+  };
+
+  const deleteMemo = id => {
+    history.push(`/delete/${id}`);
+  };
+
   return (
     <S.Container>
-      <S.MemoDetail>MemoDetail</S.MemoDetail>
+      <S.HeadWrapper>
+        <S.Backward src={backSVG} onClick={toList} />
+      </S.HeadWrapper>
+      <S.Memo>
+        <S.MemoBody>
+          {memoContents.map(contentName => (
+            <S.MemoRow key={`row${contentName}`}>
+              <S.MemoHead>{contentName.toUpperCase()}</S.MemoHead>
+              <S.MemoData>
+                {'tags' === contentName ? <TagBadgeBar tags={memo[contentName]} /> : memo[contentName]}
+              </S.MemoData>
+            </S.MemoRow>
+          ))}
+        </S.MemoBody>
+      </S.Memo>
+      <S.FootWrapper>
+        <ActionButton src={editSVG} onClick={() => editMemo(id)} />
+        <ActionButton src={deleteSVG} onClick={() => deleteMemo(id)} />
+      </S.FootWrapper>
     </S.Container>
   );
 };
---
+++ src/Pages/MemoDetail/hooks/index.js
@@ -0,0 +1,5 @@
+import useMemoDetail from './useMemoDetail';
+
+export {
+  useMemoDetail
+};
