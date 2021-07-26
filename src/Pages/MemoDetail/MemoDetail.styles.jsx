8b214398b0817a9cc3dcd90955f0168a7113e843
psy082 <zhappy114@gmail.com>
2021-07-26 18:29:30 +0900

    fix: memoEditor action 이후 페이지 이동 로직 변경

--- src/Pages/MemoDetail/MemoDetail.styles.jsx
+++ src/Pages/MemoDetail/MemoDetail.styles.jsx
@@ -32,6 +32,7 @@ export const MemoRow = styled.tr`
 
 export const MemoHead = styled.th`
   width: 20%;
+  border-right: 2px solid #70502667;
 `;
 
 export const MemoData = styled.td`
@@ -45,6 +46,10 @@ export const MemoData = styled.td`
     border: none;
     background: #f5f5f5;
   }
+`;
+
+export const Text = styled.div`
+  padding: 0 9px;
 `;
 
 export const FootWrapper = styled.div`
